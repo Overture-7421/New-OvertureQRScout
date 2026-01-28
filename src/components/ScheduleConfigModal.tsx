d33c22e383b4d5214b8f8ee713c6beee45301633
SantiQ <a01571222@itesm.mx>
2026-01-27 20:46:58 -0600

    Update

--- src/components/ScheduleConfigModal.tsx
+++ src/components/ScheduleConfigModal.tsx
@@ -1,3 +1,4 @@
+// src/components/ScheduleConfigModal.tsx
 import { useState, useEffect } from 'react';
 import type { EventConfig, Personnel, ShiftConfig, GeneratedSchedule, ScheduleConstraints, ScheduleValidationError } from '../types';
 import {
